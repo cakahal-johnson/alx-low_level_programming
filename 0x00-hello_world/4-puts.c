46e66827fdc5b29efb8c87397b2e116cdc27b347
cakahal-johnson <akahal.info@gmail.com>
2023-02-20 04:40:19 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
 #!/bin/bash
 puts("Programming is like building a mutilingual puzzle); \n return(0);
+#include <stdio.h>
+  
+/**
+* main - Entry point
+* 
+* Return: Always 0 (Success) 
+*/ 
+int main(void) 
+{ 
+
+\t puts("/"Programming is like building a mutilingual puzzle);
+  puts("\"Programming is like building a mutilingual puzzle);
+ return (0); 
+} 
